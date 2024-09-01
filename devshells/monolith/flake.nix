aecf8d21b265077c129934ae6abe77d9ddb0ae4a
Olly Swanson <42551149+ollyswanson@users.noreply.github.com>
2024-09-01 23:11:30 +0100

    feat: devshells/monolith
    Still got packages to add, but this is a barebones set of packages to
    get going.

---
+++ devshells/monolith/flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Monlith dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+
+          nativeBuildInputs = with pkgs; [ python39 poetry pyright libffi ];
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            inherit nativeBuildInputs;
+          };
+        }
+      );
+}
