1682c4a9b70e9b74ec181f63bda9f838ec36a7e5
Olly Swanson <42551149+ollyswanson@users.noreply.github.com>
2026-03-14 16:51:08 +0000

    feat(starship): Adopt bracketed-segments preset and clean up config
    Replace verbose per-language bracket format overrides with the
    bracketed-segments preset. Enable transience and direnv module,
    bump command_timeout, and fix jj-starship symbol spacing.

--- home-manager/modules/starship.nix
+++ home-manager/modules/starship.nix
@@ -5,58 +5,16 @@
   programs.starship = {
     enable = true;
     enableFishIntegration = true;
+    enableTransience = true;
+    presets = [ "bracketed-segments" ];
     settings = {
       add_newline = false;
+      command_timeout = 1000;
       directory = {
         truncation_length = 3;
         truncation_symbol = "…/";
       };
       aws.disabled = true;
-      bun = {
-        format = ''\[[$symbol($version)]($style)\]'';
-      };
-      c = {
-        format = ''\[[$symbol($version(-$name))]($style)\]'';
-      };
-      cmake = {
-        format = ''\[[$symbol($version)]($style)\]'';
-      };
-      cmd_duration = {
-        format = ''\[[⏱ $duration]($style)\]'';
-      };
-      cobol = {
-        format = ''\[[$symbol($version)]($style)\]'';
-      };
-      conda = {
-        format = ''\[[$symbol$environment]($style)\]'';
-      };
-      crystal = {
-        format = ''\[[$symbol($version)]($style)\]'';
-      };
-      daml = {
-        format = ''\[[$symbol($version)]($style)\]'';
-      };
-      dart = {
-        format = ''\[[$symbol($version)]($style)\]'';
-      };
-      deno = {
-        format = ''\[[$symbol($version)]($style)\]'';
-      };
-      docker_context = {
-        format = ''\[[$symbol$context]($style)\]'';
-      };
-      dotnet = {
-        format = ''\[[$symbol($version)(🎯 $tfm)]($style)\]'';
-      };
-      elixir = {
-        format = ''\[[$symbol($version \(OTP $otp_version\))]($style)\]'';
-      };
-      elm = {
-        format = ''\[[$symbol($version)]($style)\]'';
-      };
-      erlang = {
-        format = ''\[[$symbol($version)]($style)\]'';
-      };
       gcloud.disabled = true;
       # Built-in git modules are replaced by jj-starship, which handles
       # both jj and pure-git repos with a single optimized binary.
@@ -69,131 +27,18 @@
 
       custom.jj = {
         when = "jj-starship detect";
-        shell = [ "jj-starship"  "--jj-symbol" " 󱗆  " ];
+        shell = [ "jj-starship" "--jj-symbol" " 󱗆 " ];
         command = "prompt";
         format = " $output";
       };
-      golang = {
-        format = ''\[[$symbol($version)]($style)\]'';
-      };
-      guix_shell = {
-        format = ''\[[$symbol]($style)\]'';
-      };
-      haskell = {
-        format = ''\[[$symbol($version)]($style)\]'';
-      };
-      haxe = {
-        format = ''\[[$symbol($version)]($style)\]'';
-      };
-      helm = {
-        format = ''\[[$symbol($version)]($style)\]'';
-      };
-      hg_branch = {
-        format = ''\[[$symbol$branch]($style)\]'';
-      };
-      java = {
-        format = ''\[[$symbol($version)]($style)\]'';
-      };
-      julia = {
-        format = ''\[[$symbol($version)]($style)\]'';
-      };
-      kotlin = {
-        format = ''\[[$symbol($version)]($style)\]'';
-      };
-      kubernetes = {
-        format = ''\[[$symbol$context( \($namespace\))]($style)\]'';
-      };
-      lua = {
-        format = ''\[[$symbol($version)]($style)\]'';
-      };
-      memory_usage = {
-        format = ''\[$symbol[$ram( | $swap)]($style)\]'';
-      }; 
-      meson = {
-        format = ''\[[$symbol$project]($style)\]'';
-      };
-      nim = {
-        format = ''\[[$symbol($version)]($style)\]'';
+
+      direnv = {
+        format = ''\[[$symbol$loaded/$allowed]($style)\]'';
       };
       nix_shell = {
         symbol = "❄️nix";
         format = ''\[[$symbol]($style)\]'';
-      };
-      nodejs = {
-        format = ''\[[$symbol($version)]($style)\]'';
-      };
-      ocaml = {
-        format = ''\[[$symbol($version)(\($switch_indicator$switch_name\))]($style)\]'';
-      };
-      opa = {
-        format = ''\[[$symbol($version)]($style)\]'';
-      };
-      openstack = {
-        format = ''\[[$symbol$cloud(\($project\))]($style)\]'';
-      };
-      os = {
-        format = ''\[[$symbol]($style)\]'';
-      };
-      package = {
-        format = ''\[[$symbol$version]($style)\]'';
-      };
-      perl = {
-        format = ''\[[$symbol($version)]($style)\]'';
-      };
-      php = {
-        format = ''\[[$symbol($version)]($style)\]'';
-      };
-      pulumi = {
-        format = ''\[[$symbol$stack]($style)\]'';
-      };
-      purescript = {
-        format = ''\[[$symbol($version)]($style)\]'';
-      };
-      python = {
-        format = ''\[[''${symbol}''${pyenv_prefix}(''${version})(\($virtualenv\))]($style)\]'';
-      };
-      raku = {
-        format = ''\[[$symbol($version-$vm_version)]($style)\]'';
-      };
-      red = {
-        format = ''\[[$symbol($version)]($style)\]'';
-      };
-      ruby = {
-        format = ''\[[$symbol($version)]($style)\]'';
-      };
-      rust = {
-        format = ''\[[$symbol($version)]($style)\]'';
-      };
-      scala = {
-        format = ''\[[$symbol($version)]($style)\]'';
-      };
-      spack = {
-        format = ''\[[$symbol$environment]($style)\]'';
-      };
-      sudo = {
-        format = ''\[[as $symbol]\]'';
-      };
-      swift = {
-        format = ''\[[$symbol($version)]($style)\]'';
-      };
-      terraform = {
-        format = ''\[[$symbol$workspace]($style)\]'';
-      };
-      time = {
-        format = ''\[[$time]($style)\]'';
-      };
-      username = {
-        format = ''\[[$user]($style)\]'';
-      };
-      vagrant = {
-        format = ''\[[$symbol($version)]($style)\]'';
-      };
-      vlang = {
-        format = ''\[[$symbol($version)]($style)\]'';
-      };
-      zig = {
-        format = ''\[[$symbol($version)]($style)\]'';
-      };
+       };
     };
   };
 }
